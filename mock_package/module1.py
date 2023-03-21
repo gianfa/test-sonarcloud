02f4191e592d63a3f0bae30e2e5f8a3e983bc760
gianfa <gian.angelini@hotmail.com>
2023-03-21 22:22:22 +0100

    test(SC, qgates): insert bug; + wflow

--- mock_package/module1.py
+++ mock_package/module1.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def greetings(who:str='there') -> str:
     """ Greetings
     This is a greeting function!
@@ -17,3 +20,7 @@ def greetings(who:str='there') -> str:
     lambda printa: print("this is a code smell")
     lambda printa_2: print("this is another code smell")  
     return greet
+
+def broken_function(var: Any):
+    """ This is just broken"""
+    if 
